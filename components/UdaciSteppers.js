2ef2d6bd93e504c14f3c6e57fb217fa571bdeb24
Max Boguslavskiy <max.bogus@gmail.com>
2018-11-15 02:27:46 +0300

    implemented UdaciSlider and UdaciSteppers; added them to AddEntry

---
+++ components/UdaciSteppers.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {Text, View} from 'react-native'
+
+export default function UdaciSteppers() {
+    return (
+        <View>
+            <Text>
+                UdaciSteppers
+            </Text>
+        </View>
+    )
+}
