db78a9c8d62f49280bc17a83837c7f73bd9f9744
Max Boguslavskiy <max.bogus@gmail.com>
2018-11-15 01:57:41 +0300

    removed some redundant data

--- App.js
+++ App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Platform, StatusBar, StyleSheet, View } from 'react-native';
 import { AppLoading, Asset, Font, Icon } from 'expo';
-import AppNavigator from './navigation/AppNavigator';
 import { Ionicons } from '@expo/vector-icons';
 
 export default class App extends React.Component {
